48f87c7d23ed84005e7ff902a7478919d52a0116
FelipeJaramillo514 <bayron.jaramillo@campusucc.edu.co>
2026-04-16 09:23:08 -0500

    feat: add asistencia views and routes

--- asistencia/views.py
+++ asistencia/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import AsistenciaForm
+
+
+def registro_asistencia(request):
+    if request.method == "POST":
+        form = AsistenciaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("asistencia:confirmacion")
+    else:
+        form = AsistenciaForm()
+
+    return render(request, "asistencia/formulario.html", {"form": form})
+
+
+def confirmacion_asistencia(request):
+    return render(request, "asistencia/confirmacion.html")
 
 # Create your views here.
